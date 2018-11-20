9368cb9cba19330f0419abc7ed06dcd34b05eea7
BigBricks <BSA6.23.94@gmail.com>
2018-11-20 16:47:51 -0500

    changed a bit of home and counter nothing crazy

--- ReactASP/ClientApp/src/components/Counter.js
+++ ReactASP/ClientApp/src/components/Counter.js
@@ -6,12 +6,8 @@ import { actionCreators } from '../store/Counter';
 const Counter = props => (
   <div>
     <h1>Counter</h1>
-
-    <p>This is a simple example of a React component.</p>
-
     <p>Current count: <strong>{props.count}</strong></p>
-
-    <button onClick={props.increment}>Increment</button>
+    <button onClick={props.increment}>Increment Me</button>
   </div>
 );
 
